79f7f72dc8ee179b2d402359ae605cd6822834da
Bruno Furlanetto <bruno.furlanetto@hotmail.com>
2023-11-30 22:58:14 -0300

    Main restructuring

--- main.py
+++ main.py
@@ -1,48 +1,30 @@
-import locale
+from get_remote_files import get_test_files
+
+import os
 import subprocess
 from time import sleep
 
-from get_remote_files import get_test_files
 
-linguage = locale.getlocale()[0]
+def connect_esp(port):
+    print('Conectando com o microcontrolador...')
+    sleep(1)
+    print('Ao entrar, pressione "ctrl + B" e execute execfile("/initial/initialization.py") para dar início ao protocolo de coleta')
+    sleep(1)
+    python_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'venv\\Scripts\\python')
 
-if linguage == 'pt_BR':
+    try:
+        subprocess.call(f'{python_path} -m serial.tools.miniterm {port} 115200')
+    except Exception as e:
+        print(f'Erro {e}, na conexão com o microcontrolador')
+
+
+def main():
     port = input('Insira a porta de comunicação com o ESP32: ')
-
-    while True:
-        print('Conectando com o microcontrolador...')
-        sleep(2)
-        print('Ao entrar, pressione ctrl + B e execute execfile("/initial/initialization.py") para dar inicio ao protocolo de coleta')
-        sleep(2)
-
-        try:
-            subprocess.call(f'python -m serial.tools.miniterm {port} 115200')
-
-            break
-        except Exception as e:
-            print(f'Erro {e}, na conexão com o microcontrolador')
+    connect_esp(port)
 
     name = input('Digite o nome do voluntário: ').strip().replace(' ', '_')
-else:
-    port = input('Enter the communication port with ESP32:')
+    get_test_files(port, name.lower().replace(' ', '_'))
 
-    while True:
-        print('Connecting with the microcontroller...')
-        sleep(2)
-        print(
-            'Upon entering, press ctrl + B and execute execfile("/initial/initialization.py") to start the collection protocol')
-        sleep(2)
 
-        try:
-            subprocess.call(f'python -m serial.tools.miniterm {port} 115200')
-
-            break
-        except Exception as e:
-            if linguage == 'pt_BR':
-                print(f'Erro {e}, na conexão com o microcontrolador')
-            else:
-                print(f'Error {e}, when connecting to the microcontroller')
-
-    name = input('Enter the name of the volunteer:').strip().replace(' ', '_')
-
-get_test_files(port, name.lower().replace(' ', '_'))
+if __name__ == "__main__":
+    main()
