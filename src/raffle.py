3a72333167dd9a2b7637353fdebc57a9eded2c86
Bruno Furlanetto <bruno.furlanetto@hotmail.com>
2024-05-12 11:47:36 -0300

    Adding the script to randomize the test

---
+++ src/raffle.py
@@ -0,0 +1,12 @@
+from random import sample
+
+sensory_pathway = sample([1, 2, 3], 3)
+
+first_sequence = sample([1, 2], 2)
+second_sequence = sample([1, 2], 2)
+third_sequence = sample([1, 2], 2)
+
+print(f'Sequêcia das vias sensoriais: {sensory_pathway}')
+print(f'Sequência primeiro teste: {first_sequence}')
+print(f'Sequêcia segundo teste: {second_sequence}')
+print(f'Sequêcia terceiro teste: {third_sequence}')
